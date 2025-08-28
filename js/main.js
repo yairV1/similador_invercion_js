17487dbbe8ded706bdd040817f5f7ab08adf4127
Yair <yandrey2007@gmail.com>
2025-08-28 14:06:49 -0500

    Ya queda con las nuevas funciones del js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-function simulador(){
+function simulador() {
     const contPre = document.getElementById("pre-simulation");
     const contPost = document.getElementById("post-simulation");
-    
+
     contPost.classList.remove('disabled');
     contPre.classList.add('disabled');
 
     // los valores del campo
     let cNombre = document.getElementById("nombre").value;
-    let cTelefono= document.getElementById("telefono").value;
+    let cTelefono = document.getElementById("telefono").value;
     let cEmail = document.getElementById("email").value;
     let cMonto = document.getElementById("monto").value;
     let cTiempo = document.getElementById("tiempo").value;
@@ -24,42 +24,48 @@ function simulador(){
     let ganancia = 0;
     let total = 0;
 
-    switch (cTiempo) {
-        case '1':
-            // en este case realizaremos el calculo si el tiempo coresponde a un año
-            ganancia = (cMonto * 0.008)*12;
-            total = parseInt(cMonto) + parseInt(ganancia);
-             
-            sInteres.textContent = '0.8';
-            sTiempo.textContent ='12 Meses';
-            break;
-        
-        case '2':
-            // en este case realizaremos el calculo si el tiempo coresponde a un dos años
-            ganancia = (cMonto*0.013)*24;
-            total = parseInt(cMonto) + parseInt(ganancia);
 
-            sInteres.textContent = '1.3';
-            sTiempo.textContent ='24 Meses';
-            break;
-
-        case '3':
-            // en este case realizaremos el calculo si el tiempo coresponde a tres años
-            ganancia =(cMonto*0.017)*36;
-            total = parseInt(cMonto) + parseInt(ganancia);
-
-            sInteres.textContent = '1.7';
-            sTiempo.textContent ='36 Meses';
-            break;
+        if (cNombre === "" || cEmail === "" || cTelefono === "" || cMonto === "" || cTiempo === "") {
+            alert("Completa todos los campos")
+        }else{
             
-    }
-    sTotal.innerHTML = total;
-    sGanancia.innerHTML = ganancia;
-    sNombres.textContent = cNombre;
-    sEmail.textContent = cEmail;
-   
-}
+        switch (cTiempo) {
+            case '1':
+                // en este case realizaremos el calculo si el tiempo coresponde a un año
+                ganancia = (cMonto * 0.008) * 12;
+                total = parseInt(cMonto) + parseInt(ganancia);
 
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('calcular').addEventListener('click',simulador);
+                sInteres.textContent = '0.8';
+                sTiempo.textContent = '12 Meses';
+                break;
+
+            case '2':
+                // en este case realizaremos el calculo si el tiempo coresponde a un dos años
+                ganancia = (cMonto * 0.013) * 24;
+                total = parseInt(cMonto) + parseInt(ganancia);
+
+                sInteres.textContent = '1.3';
+                sTiempo.textContent = '24 Meses';
+                break;
+
+            case '3':
+                // en este case realizaremos el calculo si el tiempo coresponde a tres años
+                ganancia = (cMonto * 0.017) * 36;
+                total = parseInt(cMonto) + parseInt(ganancia);
+
+                sInteres.textContent = '1.7';
+                sTiempo.textContent = '36 Meses';
+                break;
+
+        }
+        sTotal.innerHTML = total;
+        sGanancia.innerHTML = ganancia;
+        sNombres.textContent = cNombre;
+        sEmail.textContent = cEmail;
+        }
+    }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('calcular').addEventListener('click', simulador);
 });
